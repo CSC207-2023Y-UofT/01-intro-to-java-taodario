b98035290ea11064dbb5fd491bba88ecfce37fff
DT <dariotaoasus@gmail.com>
2023-06-09 20:46:15 -0400

    Completed all to-do code

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,7 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,6 +100,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -143,6 +147,10 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        String[] temp_array = to_split.split(" ");
+        for (String letter : temp_array){
+            ret.append(letter.charAt(0));
+        }
 
         return ret.toString();
     }
@@ -170,6 +178,15 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        if (arr.length == 0 || arr.length == 1) {  // check if length of array is 0 or 1.
+            return current_sum;
+        } else {
+            for (int i = 1; i <= arr.length; i++){  // terminates when array is fully looped
+                if (i % 2 != 0){  // check if the index is odd
+                    current_sum = current_sum + arr[i];
+                }
+            }
+        }
 
         return current_sum;
     }
